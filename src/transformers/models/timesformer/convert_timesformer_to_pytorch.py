d72ec9ab22f12b870568dedb8ae6669bb1f5ecd7
fcakyon <fcakyon@gmail.com>
2022-12-11 10:36:18 +0300

    update timesformer weight conversion script

--- src/transformers/models/timesformer/convert_timesformer_to_pytorch.py
+++ src/transformers/models/timesformer/convert_timesformer_to_pytorch.py
@@ -12,42 +12,106 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Convert TimeSformer checkpoints from the original repository: https://github.com/MCG-NJU/TimeSformer"""
+"""Convert TimeSformer checkpoints from the original repository: https://github.com/facebookresearch/TimeSformer"""
 
 import argparse
 import json
+import os
+from pathlib import Path
 
 import numpy as np
 import torch
 
-import gdown
 from huggingface_hub import hf_hub_download
-from transformers import TimesformerConfig, TimesformerForVideoClassification, VideoMAEFeatureExtractor
+from transformers import TimesformerConfig, TimesformerForVideoClassification, VideoMAEImageProcessor
+
+HF_WRITE_TOKEN = "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"
+
+
+ORIGINAL_FILENAME_TO_INFO = {
+    "TimeSformer_divST_8x32_224_K400.pyth": {
+        "hf_name": "timesformer-base-finetuned-k400",
+        "expected_output_shape": torch.Size([1, 400]),
+        "expected_top5_pred_ids": [5, 356, 240, 134, 219],
+    },
+    "TimeSformer_divST_96x4_224_K400.pyth": {
+        "hf_name": "timesformer-large-finetuned-k400",
+        "expected_output_shape": torch.Size([1, 400]),
+        "expected_top5_pred_ids": [5, 134, 356, 142, 87],
+    },
+    "TimeSformer_divST_16x16_448_K400.pyth": {
+        "hf_name": "timesformer-hr-finetuned-k400",
+        "expected_output_shape": torch.Size([1, 400]),
+        "expected_top5_pred_ids": [5, 356, 134, 219, 240],
+    },
+    "TimeSformer_divST_8x32_224_K600.pyth": {
+        "hf_name": "timesformer-base-finetuned-k600",
+        "expected_output_shape": torch.Size([1, 600]),
+        "expected_top5_pred_ids": [9, 189, 527, 198, 74],
+    },
+    "TimeSformer_divST_96x4_224_K600.pyth": {
+        "hf_name": "timesformer-large-finetuned-k600",
+        "expected_output_shape": torch.Size([1, 600]),
+        "expected_top5_pred_ids": [9, 527, 189, 305, 366],
+    },
+    "TimeSformer_divST_16x16_448_K600.pyth": {
+        "hf_name": "timesformer-hr-finetuned-k600",
+        "expected_output_shape": torch.Size([1, 600]),
+        "expected_top5_pred_ids": [9, 527, 327, 305, 74],
+    },
+    "TimeSformer_divST_8_224_SSv2.pyth": {
+        "hf_name": "timesformer-base-finetuned-ssv2",
+        "expected_output_shape": torch.Size([1, 174]),
+        "expected_top5_pred_ids": [56, 17, 125, 15, 126],
+    },
+    "TimeSformer_divST_64_224_SSv2.pyth": {
+        "hf_name": "timesformer-large-finetuned-ssv2",
+        "expected_output_shape": torch.Size([1, 174]),
+        "expected_top5_pred_ids": [15, 139, 14, 146, 151],
+    },
+    "TimeSformer_divST_16_448_SSv2.pyth": {
+        "hf_name": "timesformer-hr-finetuned-ssv2",
+        "expected_output_shape": torch.Size([1, 174]),
+        "expected_top5_pred_ids": [98, 112, 160, 56, 66],
+    },
+}
 
 
 def get_timesformer_config(model_name):
     config = TimesformerConfig()
 
-    if "large" in model_name:
+    if "large" in model_name and "ssv2" not in model_name:
         config.num_frames = 96
-
-    if "hr" in model_name:
+        config.image_size = 224
+    elif "large" in model_name and "ssv2" in model_name:
+        config.num_frames = 64
+        config.image_size = 224
+    elif "hr" in model_name:
         config.num_frames = 16
         config.image_size = 448
+    else:
+        config.num_frames = 8
+        config.image_size = 224
 
-    repo_id = "huggingface/label-files"
     if "k400" in model_name:
         config.num_labels = 400
+        repo_id = "huggingface/label-files"
         filename = "kinetics400-id2label.json"
+        id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
     elif "k600" in model_name:
         config.num_labels = 600
         filename = "kinetics600-id2label.json"
+        repo_id = "fcakyon/label-files"
+        filename = "kinetics600-id2label.json"
+        id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
     elif "ssv2" in model_name:
         config.num_labels = 174
+        repo_id = "huggingface/label-files"
         filename = "something-something-v2-id2label.json"
+        id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
     else:
         raise ValueError("Model name should either contain 'k400', 'k600' or 'ssv2'.")
-    id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
+
     id2label = {int(k): v for k, v in id2label.items()}
     config.id2label = id2label
     config.label2id = {v: k for k, v in id2label.items()}
@@ -125,129 +189,116 @@ def convert_state_dict(orig_state_dict, config):
     return orig_state_dict
 
 
+def sample_frames_from_video_file(file_path: str, num_frames: int = 16, frame_sampling_rate=1):
+    from decord import VideoReader
+
+    videoreader = VideoReader(file_path)
+    videoreader.seek(0)
+
+    # sample frames
+    start_idx = 0
+    end_idx = num_frames * frame_sampling_rate - 1
+    indices = np.linspace(start_idx, end_idx, num=num_frames, dtype=np.int64)
+    frames = videoreader.get_batch(indices).asnumpy()
+
+    return frames
+
+
 # We will verify our results on a video of eating spaghetti
-# Frame indices used: [164 168 172 176 181 185 189 193 198 202 206 210 215 219 223 227]
-def prepare_video():
-    file = hf_hub_download(
-        repo_id="hf-internal-testing/spaghetti-video", filename="eating_spaghetti.npy", repo_type="dataset"
-    )
-    video = np.load(file)
-    return list(video)
+def prepare_video(num_frames: int = 8):
+    video_path = hf_hub_download(repo_id="nateraw/video-demo", filename="archery.mp4", repo_type="dataset")
+    frames = sample_frames_from_video_file(video_path, num_frames=num_frames, frame_sampling_rate=2)
+    return list(frames)
 
 
-def convert_timesformer_checkpoint(checkpoint_url, pytorch_dump_folder_path, model_name, push_to_hub):
+def convert_timesformer_checkpoint(original_weight_path, pytorch_dump_folder_path, model_info, push_to_hub=False):
+    model_name = model_info["hf_name"]
+
     config = get_timesformer_config(model_name)
 
     model = TimesformerForVideoClassification(config)
 
-    # download original checkpoint, hosted on Google Drive
-    output = "pytorch_model.bin"
-    gdown.cached_download(checkpoint_url, output, quiet=False)
-    files = torch.load(output, map_location="cpu")
-    if "model" in files:
-        state_dict = files["model"]
-    elif "module" in files:
-        state_dict = files["module"]
+    state_dict = torch.load(original_weight_path, map_location="cpu")
+    if "model" in state_dict:
+        state_dict = state_dict["model"]
+    elif "module" in state_dict:
+        state_dict = state_dict["module"]
     else:
-        state_dict = files["model_state"]
+        state_dict = state_dict["model_state"]
     new_state_dict = convert_state_dict(state_dict, config)
 
     model.load_state_dict(new_state_dict)
     model.eval()
 
+    # set processor
+    image_processor = VideoMAEImageProcessor(
+        image_mean=[0.45, 0.45, 0.45],
+        image_std=[0.225, 0.225, 0.225],
+        size={"shortest_edge": 448} if "hr" in model_name else {"shortest_edge": 224},
+        crop_size={"height": 448, "width": 448} if "hr" in model_name else {"height": 224, "width": 224},
+    )
+
     # verify model on basic input
-    feature_extractor = VideoMAEFeatureExtractor(image_mean=[0.5, 0.5, 0.5], image_std=[0.5, 0.5, 0.5])
-    video = prepare_video()
-    inputs = feature_extractor(video[:8], return_tensors="pt")
+    video = prepare_video(num_frames=config.num_frames)
+    inputs = image_processor(video, return_tensors="pt")
 
     outputs = model(**inputs)
     logits = outputs.logits
+    assert model_info["expected_output_shape"] == logits.shape
 
-    model_names = [
-        # Kinetics-400 checkpoints (hr = high resolution input of 448px instead of 224px)
-        "timesformer-base-finetuned-k400",
-        "timesformer-large-finetuned-k400",
-        "timesformer-hr-finetuned-k400",
-        # Kinetics-600 checkpoints (hr = high resolution input of 448px instead of 224px)
-        "timesformer-base-finetuned-k600",
-        "timesformer-large-finetuned-k600",
-        "timesformer-hr-finetuned-k600",
-        # Something-Something-v2 checkpoints (hr = high resolution input of 448px instead of 224px)
-        "timesformer-base-finetuned-ssv2",
-        "timesformer-large-finetuned-ssv2",
-        "timesformer-hr-finetuned-ssv2",
-    ]
-
-    # NOTE: logits were tested with image_mean and image_std equal to [0.5, 0.5, 0.5] and [0.5, 0.5, 0.5]
-    if model_name == "timesformer-base-finetuned-k400":
-        expected_shape = torch.Size([1, 400])
-        expected_slice = torch.tensor([-0.3016, -0.7713, -0.4205])
-    elif model_name == "timesformer-base-finetuned-k600":
-        expected_shape = torch.Size([1, 600])
-        expected_slice = torch.tensor([-0.7267, -0.7466, 3.2404])
-    elif model_name == "timesformer-base-finetuned-ssv2":
-        expected_shape = torch.Size([1, 174])
-        expected_slice = torch.tensor([-0.9059, 0.6433, -3.1457])
-    elif model_name == "timesformer-large-finetuned-k400":
-        expected_shape = torch.Size([1, 400])
-        expected_slice = torch.tensor([0, 0, 0])
-    elif model_name == "timesformer-large-finetuned-k600":
-        expected_shape = torch.Size([1, 600])
-        expected_slice = torch.tensor([0, 0, 0])
-    elif model_name == "timesformer-large-finetuned-ssv2":
-        expected_shape = torch.Size([1, 174])
-        expected_slice = torch.tensor([0, 0, 0])
-    elif model_name == "timesformer-hr-finetuned-k400":
-        expected_shape = torch.Size([1, 400])
-        expected_slice = torch.tensor([-0.9617, -3.7311, -3.7708])
-    elif model_name == "timesformer-hr-finetuned-k600":
-        expected_shape = torch.Size([1, 600])
-        expected_slice = torch.tensor([2.5273, 0.7127, 1.8848])
-    elif model_name == "timesformer-hr-finetuned-ssv2":
-        expected_shape = torch.Size([1, 174])
-        expected_slice = torch.tensor([-3.6756, -0.7513, 0.7180])
-    else:
-        raise ValueError(f"Model name not supported. Should be one of {model_names}")
-
-    # verify logits
-    assert logits.shape == expected_shape
-    assert torch.allclose(logits[0, :3], expected_slice, atol=1e-4)
-    print("Logits ok!")
+    # verify predictions
+    topk_probs = logits.softmax(dim=-1).topk(5, dim=-1)
+    topk_labels = [model.config.id2label[idx] for idx in topk_probs.indices[0].tolist()]
+    assert model_info["expected_top5_pred_ids"] == topk_probs.indices[0].tolist()
 
     if pytorch_dump_folder_path is not None:
-        print(f"Saving model and feature extractor to {pytorch_dump_folder_path}")
-        feature_extractor.save_pretrained(pytorch_dump_folder_path)
+        print(f"Saving model and processor to {pytorch_dump_folder_path}")
+
+        image_processor.save_pretrained(pytorch_dump_folder_path)
         model.save_pretrained(pytorch_dump_folder_path)
 
     if push_to_hub:
         print("Pushing to the hub...")
-        model.push_to_hub(f"fcakyon/{model_name}")
+        model.push_to_hub(f"fcakyon/{model_name}", use_auth_token=HF_WRITE_TOKEN)
+        image_processor.push_to_hub(f"fcakyon/{model_name}", use_auth_token=HF_WRITE_TOKEN)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Required parameters
     parser.add_argument(
-        "--checkpoint_url",
-        default="https://drive.google.com/u/1/uc?id=17yvuYp9L4mn-HpIcK5Zo6K3UoOy1kA5l&export=download",
+        "--weights_folder_dir",
+        default=".",
         type=str,
-        help=(
-            "URL of the original PyTorch checkpoint (on Google Drive) you'd like to convert. Should be a direct"
-            " download link."
-        ),
+        help=("Folder that contains the TimeSformer weigh files."),
     )
     parser.add_argument(
-        "--pytorch_dump_folder_path",
-        default="",
+        "--output_folder_dir",
+        default="timesformer_exports/",
         type=str,
         help="Path to the output PyTorch model directory.",
     )
-    parser.add_argument("--model_name", default="timesformer-base-finetuned-k400", type=str, help="Name of the model.")
     parser.add_argument(
         "--push_to_hub", action="store_true", help="Whether or not to push the converted model to the 🤗 hub."
     )
 
     args = parser.parse_args()
-    convert_timesformer_checkpoint(
-        args.checkpoint_url, args.pytorch_dump_folder_path, args.model_name, args.push_to_hub
-    )
+
+    files = os.listdir(args.weights_folder_dir)
+    weight_files = [file for file in files if file.endswith(".pyth")]
+
+    if len(weight_files) == 0:
+        raise ValueError(
+            "The weights_folder_dir should contain at least one file."
+            "Please check that the folder exists and contains the weights files."
+        )
+
+    for file in files:
+        if file.endswith(".pyth"):
+            model_name = ORIGINAL_FILENAME_TO_INFO[file]["hf_name"]
+            convert_timesformer_checkpoint(
+                original_weight_path=os.path.join(args.weights_folder_dir, file),
+                pytorch_dump_folder_path=os.path.join(args.output_folder_dir, model_name),
+                model_info=ORIGINAL_FILENAME_TO_INFO[file],
+                push_to_hub=args.push_to_hub,
+            )
